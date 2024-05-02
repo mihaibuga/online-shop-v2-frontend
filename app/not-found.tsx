42f647c54f35d1cb528edf0d708aa5b648ed2541
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-05-02 13:07:51 +0200

    Escaped characters in not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,14 +7,14 @@ const NotFound = async () => {
         <div className="bg-gray-100 px-2 text-center">
             <div className="h-screen flex flex-col justify-center items-center">
                 <h1 className="mb-4 text-6xl font-semibold text-red-500">404</h1>
-                <p className="mb-4 text-lg text-gray-600">Oops! Looks like you're lost.</p>
+                <p className="mb-4 text-lg text-gray-600">Oops! Looks like you&apos;re lost.</p>
                 <div className="animate-bounce">
                     <div className="mx-auto h-16 w-16 text-red-500">
                         <IoMdSend size={"100%"} style={{ transform: "rotate(-90deg)" }} />
                     </div>
                 </div>
                 <p className="mt-4 text-gray-600">
-                    Let's get you back{" "}
+                    Let&apos;s get you back{" "}
                     <Link href={URL_PATHS.HOME.path} className="text-blue-500">
                         home
                     </Link>
