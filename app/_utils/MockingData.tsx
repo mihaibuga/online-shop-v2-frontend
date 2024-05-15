434ee635940a9202fdacc74466044042a95261d3
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-05-15 14:00:34 +0200

    Created mocking data

---
+++ app/_utils/MockingData.tsx
@@ -0,0 +1,71 @@
+export const messages = [
+    {
+        authorName: "Mariya Desoja",
+        authorImgSrc: "/images/user/user-01.png",
+        messageContent: "I like your confidence 💪",
+        time: "2min ago",
+    },
+    {
+        authorName: "Robert Jhon",
+        authorImgSrc: "/images/user/user-02.png",
+        messageContent: "Can you share your offer?",
+        time: "10min ago",
+    },
+    {
+        authorName: "Henry Dholi",
+        authorImgSrc: "/images/user/user-03.png",
+        messageContent: "I came across your profile and...",
+        time: "1day ago",
+    },
+    {
+        authorName: "Cody Fisher",
+        authorImgSrc: "/images/user/user-04.png",
+        messageContent: "I’m waiting for you response!",
+        time: "5days ago",
+    },
+    {
+        authorName: "Mariya Desoja",
+        authorImgSrc: "/images/user/user-05.png",
+        messageContent: "I like your confidence",
+        time: "2min ago",
+    },
+];
+
+export const notifications = [
+    {
+        notificationContent: (
+            <>
+                Edit your information in a swipe Sint occaecat
+                cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.
+            </>
+        ),
+        time: "12 May, 2025",
+    },
+    {
+        notificationContent: (
+            <>
+                It is a long established fact that a reader will be
+                distracted by the readable.
+            </>
+        ),
+        time: "24 Feb, 2025",
+    },
+    {
+        notificationContent: (
+            <>
+                There are many variations of passages of Lorem Ipsum
+                available, but the majority have suffered
+            </>
+        ),
+        time: "04 Jan, 2025",
+    },
+    {
+        notificationContent: (
+            <>
+                <span className="text-black dark:text-white">There are many variations</span> of passages of Lorem Ipsum
+                available, but the majority have suffered
+            </>
+        ),
+        time: "01 Dec, 2024",
+    },
+];
