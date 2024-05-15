5e42d10928297a6bb327e1c39780077e8e119fdf
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-05-15 13:59:42 +0200

    Created interfaces

--- app/_utils/interfaces.d.ts
+++ app/_utils/interfaces.d.ts
@@ -1,16 +1,41 @@
+// Entities
 export interface IUser {
     id: string;
     userName: string;
     profileImage: string;
 }
 
+interface IMessage {
+    authorName: string;
+    authorImgSrc: string;
+    messageContent: string;
+    time: string;
+}
+
+interface INotification {
+    notificationContent: React.ReactNode;
+    time: string;
+}
+
+// Actions
 export interface IClickEvent {
     expandedElementRef: MutableRefObject<any>;
-    triggerRef: MutableRefObject<any>
+    triggerRef: MutableRefObject<any>;
     isTargetOpen: boolean;
     setIsTargetOpen: SetStateAction<boolean>;
 }
 export interface IKeyEvent {
     isTargetOpen: boolean;
     setIsTargetOpen: SetStateAction<boolean>;
+}
+
+// Header elements
+export interface IButtonWithDropdown {
+    icon: React.ReactNode;
+    children: React.JSX.Element;
+}
+export interface IHeaderDropdownLink {
+    triggerRef: MutableRefObject<any>;
+    setIsTargetOpen: SetStateAction<boolean>;
+    icon: React.ReactNode;
 }
