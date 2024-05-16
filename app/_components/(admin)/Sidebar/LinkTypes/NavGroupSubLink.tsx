6e1ab5f0916e4f6b9548d651ae51332638448373
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-05-16 23:07:06 +0200

    Populated admin sidebar with links

--- app/_components/(admin)/Sidebar/LinkTypes/NavGroupSubLink.tsx
+++ app/_components/(admin)/Sidebar/LinkTypes/NavGroupSubLink.tsx
@@ -6,8 +6,8 @@ const NavGroupSubLink = ({ title, linkPath, pathname }: INavGroupSubLinkProps) =
     return (
         <Link
             href={linkPath}
-            className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium text-bodydark2 duration-300 ease-in-out hover:text-white ${
-                pathname === linkPath && "text-white"
+            className={`group relative flex items-center gap-2.5 rounded-md px-4 font-medium duration-300 ease-in-out hover:bg-gray-300 ${
+                pathname === linkPath ? "text-black bg-gray-300" : "text-bodydark2"
             }`}
         >
             {title}
--- app/_components/(admin)/Sidebar/index.tsx
+++ app/_components/(admin)/Sidebar/index.tsx
@@ -4,9 +4,12 @@ import React, { useRef } from "react";
 import { usePathname } from "next/navigation";
 
 import { CiSettings } from "react-icons/ci";
-import { LuLayoutDashboard } from "react-icons/lu";
-import { MdOutlinePersonOutline } from "react-icons/md";
+import { MdOutlinePermMedia, MdOutlinePersonOutline } from "react-icons/md";
 import { RxDashboard } from "react-icons/rx";
+import { IoMdAdd } from "react-icons/io";
+import { AiOutlineMessage } from "react-icons/ai";
+import { PiBellLight } from "react-icons/pi";
+import { FaBox } from "react-icons/fa6";
 
 import useGeneralStore from "@/app/_stores/generalStore";
 import { URL_PATHS } from "@/app/_utils/constants";
@@ -54,37 +57,147 @@ const Sidebar = () => {
                             pathnameToCompare={"/"}
                             pathnameIncludes={"dashboard"}
                         >
-                            <NavGroupSubLink title={"eCommerce"} linkPath={"/"} pathname={pathname} />
+                            <NavGroupSubLink title={"Home"} linkPath={URL_PATHS.ADMIN.path} pathname={pathname} />
+                            <NavGroupSubLink
+                                title={"Updates"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/updates`}
+                                pathname={pathname}
+                            />
+                        </ExpandableMainNavLink>
+                    </MainLinksCategoryGroup>
+
+                    <MainLinksCategoryGroup groupTitle={"ENTITY TYPES"}>
+                        <ExpandableMainNavLink
+                            title={"New"}
+                            icon={<IoMdAdd size={"100%"} />}
+                            mainHref={"#"}
+                            pathname={pathname}
+                            pathnameToCompare={"/create"}
+                            pathnameIncludes={"create"}
+                        >
+                            <NavGroupSubLink
+                                title={"User"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/users/new`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Page"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/pages/new`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Media"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/media/new`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Product"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/products/new`}
+                                pathname={pathname}
+                            />
+                        </ExpandableMainNavLink>
+
+                        <ExpandableMainNavLink
+                            title={"Products"}
+                            icon={<FaBox size={"100%"} />}
+                            mainHref={"#"}
+                            pathname={pathname}
+                            pathnameToCompare={`${URL_PATHS.ADMIN.path}/products`}
+                            pathnameIncludes={"products"}
+                        >
+                            <NavGroupSubLink
+                                title={"All Products"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/products`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Add New Product"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/products/new`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Categories"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/taxonomies/categories`}
+                                pathname={pathname}
+                            />
+                        </ExpandableMainNavLink>
+
+                        <ExpandableMainNavLink
+                            title={"Media"}
+                            icon={<MdOutlinePermMedia size={"100%"} />}
+                            mainHref={"#"}
+                            pathname={pathname}
+                            pathnameToCompare={`${URL_PATHS.ADMIN.path}/media`}
+                            pathnameIncludes={"media"}
+                        >
+                            <NavGroupSubLink
+                                title={"Library"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/media`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Add New Media File"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/media/new`}
+                                pathname={pathname}
+                            />
                         </ExpandableMainNavLink>
 
                         <SimpleMainNavLink
-                            title={"Profile"}
-                            linkPath={"/profile"}
+                            title={"Messages"}
+                            linkPath={`${URL_PATHS.ADMIN.path}/messages`}
                             pathname={pathname}
-                            icon={<MdOutlinePersonOutline size={"100%"} />}
-                            pathnameIncludes={"profile"}
+                            icon={<AiOutlineMessage size={"100%"} />}
+                            pathnameIncludes={"messages"}
                         />
 
                         <SimpleMainNavLink
-                            title={"Settings"}
-                            linkPath={"/settings"}
+                            title={"Notifications"}
+                            linkPath={`${URL_PATHS.ADMIN.path}/notifications`}
                             pathname={pathname}
-                            icon={<CiSettings size={"100%"} />}
-                            pathnameIncludes={"settings"}
+                            icon={<PiBellLight size={"100%"} />}
+                            pathnameIncludes={"notifications"}
                         />
                     </MainLinksCategoryGroup>
 
-                    <MainLinksCategoryGroup groupTitle={"OTHERS"}>
+                    <MainLinksCategoryGroup groupTitle={"CONFIGURATION"}>
                         <ExpandableMainNavLink
-                            title={"UI Elements"}
-                            icon={<LuLayoutDashboard size={"100%"} />}
+                            title={"Users"}
+                            icon={<MdOutlinePersonOutline size={"100%"} />}
                             mainHref={"#"}
                             pathname={pathname}
-                            pathnameToCompare={"/ui"}
-                            pathnameIncludes={"ui"}
+                            pathnameToCompare={`${URL_PATHS.ADMIN.path}/users`}
+                            pathnameIncludes={"users"}
                         >
-                            <NavGroupSubLink title={"Alerts"} linkPath={"/ui/alerts"} pathname={pathname} />
-                            <NavGroupSubLink title={"Buttons"} linkPath={"/ui/buttons"} pathname={pathname} />
+                            <NavGroupSubLink
+                                title={"All Users"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/users`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Add New User"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/users/new`}
+                                pathname={pathname}
+                            />
+                            <NavGroupSubLink
+                                title={"Profile"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/users/profile`}
+                                pathname={pathname}
+                            />
+                        </ExpandableMainNavLink>
+
+                        <ExpandableMainNavLink
+                            title={"Settings"}
+                            icon={<CiSettings size={"100%"} />}
+                            mainHref={"#"}
+                            pathname={pathname}
+                            pathnameToCompare={`${URL_PATHS.ADMIN.path}/settings`}
+                            pathnameIncludes={"settings"}
+                        >
+                            <NavGroupSubLink
+                                title={"General"}
+                                linkPath={`${URL_PATHS.ADMIN.path}/settings/general`}
+                                pathname={pathname}
+                            />
                         </ExpandableMainNavLink>
                     </MainLinksCategoryGroup>
                 </nav>
