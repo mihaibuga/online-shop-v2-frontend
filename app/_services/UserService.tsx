9ce8dea6dd5e00cd95a0a5231e500575aec0f3a2
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-06-07 15:50:38 +0200

    Created user creation service

--- app/_services/UserService.tsx
+++ app/_services/UserService.tsx
@@ -1,9 +1,11 @@
-import { IUser } from "../_utils/interfaces";
-import { apiBaseURL, deleteData, fetchData } from "../_utils/api";
+import { AxiosRequestConfig } from "axios";
 import { toast } from "react-toastify";
 
-const headers = (token: string) => {
-    return { headers: { Authorization: `Bearer ${token}` } };
+import { IRegisterUser, IUser } from "../_utils/interfaces";
+import { apiBaseURL, deleteData, fetchData, postData } from "../_utils/api";
+
+export const headers = (token: string) => {
+    return { headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" } };
 };
 
 export const getUsers = async (token: string) => {
@@ -21,5 +23,27 @@ export const deleteUser = async (id: string, token: string) => {
         return result;
     } catch (e: any) {
         toast.error("Server error occured");
+    }
+};
+
+export const createUser = async (
+    registeringUserDetails: IRegisterUser,
+    fetchConfigs?: AxiosRequestConfig<any> | undefined
+) => {
+    try {
+        let result =
+            fetchConfigs !== undefined
+                ? await postData<IUser>(apiBaseURL + "/users", registeringUserDetails, fetchConfigs)
+                : await postData<IUser>(apiBaseURL + "/users", registeringUserDetails);
+        if (result) {
+            const user: IUser = {
+                userName: result?.data.userName,
+                email: result?.data.email,
+            };
+            return user;
+        }
+        return result;
+    } catch (e: any) {
+        toast.warning("Server error occured");
     }
 };
