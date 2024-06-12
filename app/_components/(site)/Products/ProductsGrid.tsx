47694e9b4d5ce36a7ad242763f43595de686c487
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-06-12 12:32:31 +0200

    Added icon for add to cart buttons

--- app/_components/(site)/Products/ProductsGrid.tsx
+++ app/_components/(site)/Products/ProductsGrid.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import { IoMdCart } from "react-icons/io";
 
 type ProductType = {
     src?: string;
@@ -66,8 +67,9 @@ const ProductsGrid = ({ sectionTitle, products }: IProps) => {
 
                                 <button
                                     type="button"
-                                    className="w-full px-5 py-2.5 bg-blue-600 hover:bg-blue-700 font-bold text-white rounded-lg duration-200"
+                                    className="w-full flex items-center justify-center gap-3 px-5 py-2.5 bg-blue-600 hover:bg-blue-700 font-semibold text-white rounded-xl duration-200"
                                 >
+                                    <IoMdCart size={"100%"} className="w-[20px] h-[20px]" />
                                     Add to cart
                                 </button>
                             </div>
