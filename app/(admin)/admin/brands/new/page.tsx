22dc0f8a16cfdabd7e921ac4f3760d514e908a77
Mihai Buga <77620547+mihaibuga@users.noreply.github.com>
2024-06-17 20:45:41 +0200

    Created new pages

---
+++ app/(admin)/admin/brands/new/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const NewBrandPage = (props: Props) => {
+    return (
+        <div>
+            <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white">
+                New Brand
+            </h2>
+        </div>
+    );
+};
+
+export default NewBrandPage;
---
+++ app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {};
+
+const CategoriesPage = (props: Props) => {
+    return (
+        <div>
+            <h2 className="mb-4 text-3xl font-extrabold leading-none tracking-tight text-gray-900 md:text-4xl dark:text-white">
+                Categories
+            </h2>
+        </div>
+    );
+};
+
+export default CategoriesPage;
