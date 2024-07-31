19e454a586d5b8ac485678d3b7b581a54d2a477c
Mihai Buga <mihaibuga11@gmail.com>
2024-07-31 17:16:48 +0200

    Protected the admin routes

--- app/(routes)/admin/layout.tsx
+++ app/(routes)/admin/layout.tsx
@@ -5,6 +5,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 import "@/app/(private)/_ui/styles/globals.scss";
+import ProtectedRoute from "@/app/(private)/_components/Others/ProtectedRoute";
 import MainTheme from "@/app/(private)/_themes/MainTheme/MainTheme";
 import Sidebar from "@/app/(private)/_components/Sidebars/AdminSidebar";
 import Header from "@/app/(private)/_components/Headers/AdminHeader";
@@ -23,20 +24,22 @@ const AdminLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <html lang="en">
             <body className={`${inter.className} non-scrollable`}>
-                <MainTheme>
-                    <div className="flex h-screen overflow-hidden bg-[#EEEEEE] dark:bg-[#222831]">
-                        <Sidebar />
+                <ProtectedRoute>
+                    <MainTheme>
+                        <div className="flex h-screen overflow-hidden bg-[#EEEEEE] dark:bg-[#222831]">
+                            <Sidebar />
 
-                        <div className="non-scrollable relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                            <Header />
+                            <div className="non-scrollable relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+                                <Header />
 
-                            <main className="min-h-[calc(100vh-182px)] md:min-h-[calc(100vh-72px)] w-full text-[#222831] dark:text-[#EEEEEE]">
-                                <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">{children}</div>
-                            </main>
+                                <main className="min-h-[calc(100vh-182px)] md:min-h-[calc(100vh-72px)] w-full text-[#222831] dark:text-[#EEEEEE]">
+                                    <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">{children}</div>
+                                </main>
+                            </div>
                         </div>
-                    </div>
-                    <ToastContainer />
-                </MainTheme>
+                        <ToastContainer />
+                    </MainTheme>
+                </ProtectedRoute>
             </body>
         </html>
     );
